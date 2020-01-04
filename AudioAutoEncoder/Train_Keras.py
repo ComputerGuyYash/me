ecf94b5fba1b5e47f3927d3a725e8fa5ff7b33ba
Computer-CGuy <breakdownninja@gmail.com>
2020-01-05 02:37:30 +0530

    Retrain

--- AudioAutoEncoder/Train_Keras.py
+++ AudioAutoEncoder/Train_Keras.py
@@ -2,12 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Encoded.csv')
+dataset = pd.read_csv('end.csv')
 
 X = dataset.iloc[:, :].values
 y = dataset.iloc[:, :-1].values
-X[:,0]*=-1
-y[:,0]*=-1
 
 import tensorflow as tf
 import keras
@@ -16,11 +14,11 @@ from keras.layers import Dense
 
 
 classifier = Sequential()
-classifier.add(Dense(units = 129, kernel_initializer = 'uniform', activation = 'relu', input_dim = 129))
-classifier.add(Dense(units = 56, kernel_initializer = 'uniform', activation = 'relu', input_dim = 56))
-classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 10))
-classifier.add(Dense(units = 50, kernel_initializer = 'uniform', activation = 'relu', input_dim = 50))
-classifier.add(Dense(units = 128, kernel_initializer = 'uniform', activation = 'relu', input_dim = 128))
+classifier.add(Dense(units = 18, kernel_initializer = 'uniform', activation = 'relu', input_dim = 18))
+classifier.add(Dense(units = 9, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 9, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 17, kernel_initializer = 'uniform', activation = 'relu'))
 
 
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
